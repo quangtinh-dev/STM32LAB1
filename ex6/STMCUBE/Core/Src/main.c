ae81e211398ecd85d47c874f4609ef93e0685390
DESKTOP-77VQ0AC\Tinh <tinh.trance3112@hcmut.edu.vn>
2024-11-01 08:41:43 +0700

    fix something

--- ex6/STMCUBE/Core/Src/main.c
+++ ex6/STMCUBE/Core/Src/main.c
@@ -112,18 +112,18 @@ int main(void)
 
 	  if(count == 12)// turn off all led
 	  {
-		  HAL_GPIO_WritePin(led0_GPIO_Port, led0_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led1_GPIO_Port, led1_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led2_GPIO_Port, led2_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led3_GPIO_Port, led3_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led4_GPIO_Port, led4_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led5_GPIO_Port, led5_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led6_GPIO_Port, led6_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led7_GPIO_Port, led7_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led8_GPIO_Port, led8_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led9_GPIO_Port, led9_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led10_GPIO_Port, led10_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(led11_GPIO_Port, led11_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led1_GPIO_Port, led0_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led2_GPIO_Port, led1_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led3_GPIO_Port, led2_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led4_GPIO_Port, led3_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led5_GPIO_Port, led4_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led6_GPIO_Port, led5_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led7_GPIO_Port, led6_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led8_GPIO_Port, led7_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led9_GPIO_Port, led8_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led10_GPIO_Port, led9_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led11_GPIO_Port, led10_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(led0_GPIO_Port, led11_Pin, GPIO_PIN_RESET);
 		  count = 0;
 	  }
 
